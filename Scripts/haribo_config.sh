2219d4bb35ce504fa57705dede9fa1ddd40db193
Ryan Neufeld <rneufeld@opendns.com>
2017-10-19 17:00:03 -0700

    disabling verbose output

--- Scripts/haribo_config.sh
+++ Scripts/haribo_config.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -e
+echo "Configuring for Haribo"
 sed -i '/CUSTOM_MENDEL_NAME/c\#define CUSTOM_MENDEL_NAME "3030 Haribo Edition"' Firmware/Configuration_prusa.h
 sed -i '/lcd_print_at_PGM(0, 1, PSTR("   Original Prusa   "));/c\lcd_print_at_PGM(0, 1, PSTR("    3030 Haribo     "));' Firmware/Marlin_main.cpp
 sed -i '/lcd_print_at_PGM(0, 2, PSTR("    3D  Printers    "));/c\lcd_print_at_PGM(0, 2, PSTR("      Edition       "));' Firmware/Marlin_main.cpp
